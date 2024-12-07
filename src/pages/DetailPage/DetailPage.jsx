37260580fb6e22a53fba58cac8ff545614cfdf95
Alexander-Tyukaev <Sanek4100@gmail.com>
2024-12-07 17:58:17 +0300

    stranica krasovki

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,10 +1,26 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const DetailPage = () => {
-
+    const [sneak, setSneak] = useState(null)
     const params = useParams()
     let sneakId = params.id
+
+    useEffect(() =>{
+        async function fetchData(url) {
+            try{
+                const response = await axios.get(url)
+                setSneak(response.data)
+              
+            }catch (error){
+                console.error('Error fetching :',error.message)
+            }
+        }
+        const url = 'https://03d0ddaaff43dfdf.mokky.dev/sneackers'
+        fetchData(url)
+
+    },[])
   return (
     <div>
       Detail page {sneakId} sneak
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,10 +2,13 @@ import axios from "axios";
 import React, { useEffect, useState } from "react"
 import Nouislider from "nouislider-react";
 import "nouislider/distribute/nouislider.css";
+import { useNavigate } from "react-router-dom";
+import { DETAIL_PAGE_ROUTE } from "../../router/consts";
 
 
 
 const MainPage = () => {
+    const navigate = useNavigate()
 
     const [sneackers,setSneackers] = useState(null)
 
@@ -14,6 +17,16 @@ const MainPage = () => {
 
 const [man, setMan] = useState(true)
 const [woman, setWoman] = useState(true)
+
+const [size35,setSize35] = useState(false)
+const [size36,setSize36] = useState(false)
+const [size37,setSize37] = useState(false)
+const [size38,setSize38] = useState(false)
+const [size39,setSize39] = useState(false)
+const [size40,setSize40] = useState(false)
+const [size41,setSize41] = useState(false)
+const [size42,setSize42] = useState(false)
+const [size43,setSize43] = useState(false)
 
     async function fetchData(url) {
         try{
@@ -32,6 +45,7 @@ const [woman, setWoman] = useState(true)
 
     const filter = () => {
         let gender =''
+        let sizes = ''
   
         if(man && !woman){
             gender = '&gender=Мужской'
@@ -39,10 +53,21 @@ const [woman, setWoman] = useState(true)
         else if(!man && woman){
             gender = '&gender=Женский'
         }
-        const url = 'https://03d0ddaaff43dfdf.mokky.dev/sneackers?price[from]='+ min +'&price[to]=' + max +gender
+
+        if(size35) { sizes += "&sizes[]=35"}
+        if(size36) { sizes += "&sizes[]=36"}
+        if(size37) { sizes += "&sizes[]=37"}
+        if(size38) { sizes += "&sizes[]=38"}
+        if(size39) { sizes += "&sizes[]=39"}
+        if(size40) { sizes += "&sizes[]=40"}
+        if(size41) { sizes += "&sizes[]=41"}
+        if(size42) { sizes += "&sizes[]=42"}
+        if(size43) { sizes += "&sizes[]=43"}
+
+        const url = 'https://03d0ddaaff43dfdf.mokky.dev/sneackers?price[from]='+ min +'&price[to]=' + max + gender + sizes
         fetchData(url)
     }
-  
+
 
   return (
     <div>
@@ -67,12 +92,26 @@ const [woman, setWoman] = useState(true)
         <br />
         <div><input type="checkbox" checked={man} onChange={() => setMan(!man)} />Мужской </div>
         <div><input type="checkbox" checked={woman} onChange={() => setWoman(!woman)} />Женский </div>
+        <br />
+        <div>
+            <div><input type="checkbox" checked={size35} onChange={() => setSize35(!size35)} />35 </div>
+            <div><input type="checkbox" checked={size36} onChange={() => setSize36(!size36)} />36 </div>
+            <div><input type="checkbox" checked={size37} onChange={() => setSize37(!size37)} />37 </div>
+            <div><input type="checkbox" checked={size38} onChange={() => setSize38(!size38)} />38 </div>
+            <div><input type="checkbox" checked={size39} onChange={() => setSize39(!size39)} />39 </div>
+            <div><input type="checkbox" checked={size40} onChange={() => setSize40(!size40)} />40 </div>
+            <div><input type="checkbox" checked={size41} onChange={() => setSize41(!size41)} />41 </div>
+            <div><input type="checkbox" checked={size42} onChange={() => setSize42(!size42)} />42 </div>
+            <div><input type="checkbox" checked={size43} onChange={() => setSize43(!size43)} />43 </div>
 
+        </div>
+        <br />
         <button onClick={filter}>Применить фильтры</button>
+        <br />
        {sneackers && sneackers.map(item =>
             <div key={item.id}>
-            {item.id} | {item.title} | {item.price}
-
+            {item.id} | {item.title} | {item.price} | {item.sizes.map(size => <span key = {size}>  [{ size }]  </span>)}
+        <button onClick={() => navigate(DETAIL_PAGE_ROUTE+ '/' + item.id )}>Посмотреть</button>
             </div>
        )}
     </div>
