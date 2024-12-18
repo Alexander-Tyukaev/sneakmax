c811a8c616ca8997a86231f5ae9cb576fc056871
Alexander-Tyukaev <Sanek4100@gmail.com>
2024-12-18 22:14:42 +0300

    slider nachalo3

--- src/components/slider/ShoeTypeSelection.jsx
+++ src/components/slider/ShoeTypeSelection.jsx
@@ -1,39 +1,37 @@
 import React from 'react';
+// import './ShoeTypeSelection.css'; 
+
 
 const ShoeTypeSelection = ({ shoeType, onChange }) => {
   const shoeTypeImages = {
     кроссовки: require('../image/5.png'),
-    туфли:  require('../image/4.png'),
+    туфли: require('../image/4.png'),
     сапоги: require('../image/9.png'),
     сандалии: require('../image/7.png'),
-    кеды:require('../image/6.png'),
-    тапки:require('../image/8.png'),
+    кеды: require('../image/6.png'),
+    тапки: require('../image/8.png'),
   };
 
   return (
     <div>
-        <div className="quiz-title">Мы подберем идеальную пару для вас</div>
-        <div className="quiz-text">Ответьте на три вопроса и мы вышлем каталог с самыми подходящими для вас моделями </div>
-        <div className="quiz-polosa"></div>
+      <div className="quiz-title">Мы подберем идеальную пару для вас</div>
+      <div className="quiz-text">Ответьте на три вопроса и мы вышлем каталог с самыми подходящими для вас моделями </div>
+      <div className="quiz-polosa"></div>
       <h3>Какой тип обуви вы предпочитаете?</h3>
       <div className="slider-container">
         {Object.entries(shoeTypeImages).map(([type, img]) => (
-          <label key={type} className="img-label">
+          <label key={type} className="img-label" onClick={() => onChange(type)}>
             <img src={img} alt={type} className="slaider-img" />
-            
-            <input
-              type="radio"
-              value={type}
-              checked={shoeType === type}
-              onChange={(e) => onChange(e.target.value)}
+            <div
+              div className={`slider-checkbox ${shoeType === type ? 'checked' : ''}`}
+              onClick={() => onChange(type)} 
             />
             {type}
           </label>
         ))}
-          <div className="quiz-polosa2"></div>
-          <div className="quiz-nomeracia">1 из 3</div>
+        <div className="quiz-polosa2"></div>
+        <div className="quiz-nomeracia">1 из 3</div>
       </div>
-    
     </div>
   );
 };
